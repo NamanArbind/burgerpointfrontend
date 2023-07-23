e92cddd13cd697f96f9c08ef7c4281966b8d3aaf
Naman <namanarbind6@gmail.com>
2023-07-24 02:57:33 +0530

    first commit

---
+++ src/components/MyOrders/MyOrders.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineEye } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { getMyOrders } from "../../redux/actions/order";
+import Loader from "../Layout/Loader";
+
+const MyOrders = () => {
+  const dispatch = useDispatch();
+  const { orders, loading, error } = useSelector((state) => state.orders);
+
+  useEffect(() => {
+    dispatch(getMyOrders());
+  }, [dispatch]);
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: "clearError" });
+    }
+  }, [error,dispatch]);
+
+  return (
+    <section className="tableClass">
+      {loading === false ? (
+        <main>
+          <table>
+            <thead>
+              <tr>
+                <th>Order Id</th>
+                <th>Status</th>
+                <th>Item Qty</th>
+                <th>Amount</th>
+                <th>Payment Method</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {orders &&
+                orders.map((i) => (
+                  <tr key={i._id}>
+                    <td>#{i._id} </td>
+                    <td>{i.orderStatus}</td>
+                    <td>
+                      {i.orderItems.cheeseBurger.quantity +
+                        i.orderItems.vegCheeseBurger.quantity +
+                        i.orderItems.burgerWithFries.quantity}
+                    </td>
+                    <td>₹{i.totalAmount}</td>
+                    <td>{i.paymentMethod}</td>
+                    <td>
+                      <Link to={`/order/${i._id}`}>
+                        <AiOutlineEye />
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </main>
+      ) : (
+        <Loader />
+      )}
+    </section>
+  );
+};
+
+export default MyOrders;
